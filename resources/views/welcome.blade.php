9c367673ffcffe22c30ae2e5b00c555ceefd8adf
nazyh <nazyhfakherdine@gmail.com>
2025-08-11 15:27:33 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Laravel Test Page</title>
+</head>
+<body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+    <h1>Hello, Laravel! 🎉</h1>
+    <p>This is a custom page replacing the default welcome screen.</p>
+</body>
+</html>
